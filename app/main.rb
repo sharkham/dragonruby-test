1920d113cfa8fade9143f8640450c7033e7ea0bd
Sam Markham <sam.markham@clio.com>
2024-02-29 11:55:41 -0800

    Make dragon spit fire

--- app/main.rb
+++ app/main.rb
@@ -2,44 +2,68 @@ PLAYER_W = 100
 PLAYER_H = 80
 SPEED = 12
 
-def tick args
-  args.state.player_x ||= 120
-  args.state.player_y ||= 280
+def tick(args)
+  args.state.player ||= {
+    x: 120,
+    y: 280,
+    w: PLAYER_W,
+    h: PLAYER_H,
+    speed: SPEED,
+    path: 'sprites/misc/dragon-0.png',
+  }
+  args.state.fireballs ||= []
 
   handle_movement(args)
   stay_in_bounds(args)
+  spit_fire(args)
 
-  args.outputs.sprites << [args.state.player_x, args.state.player_y, PLAYER_W, PLAYER_H, 'sprites/misc/dragon-0.png']
+  args.outputs.sprites << [args.state.player, args.state.fireballs]
+end
+
+def spit_fire(args)
+  if args.inputs.keyboard.key_down.z || args.inputs.keyboard.key_down.j || args.inputs.controller_one.key_down.a
+    args.state.fireballs << {
+      x: args.state.player.x + args.state.player.w,
+      y: args.state.player.y + (args.state.player.h/6),
+      w: 32,
+      h: 32,
+      path: 'sprites/misc/fireball.png',
+    }
+  end
+
+  args.state.fireballs.each do |fireball|
+    fireball.x += args.state.player.speed + 2
+  end
 end
 
 def handle_movement(args)
   if args.inputs.left
-    args.state.player_x -= SPEED
+    args.state.player.x -= args.state.player.speed
   elsif args.inputs.right
-    args.state.player_x += SPEED
+    args.state.player.x += args.state.player.speed
   end
 
   if args.inputs.up
-    args.state.player_y += SPEED
+    args.state.player.y += args.state.player.speed
   elsif args.inputs.down
-    args.state.player_y -= SPEED
+    args.state.player.y -= args.state.player.speed
   end
 end
 
 def stay_in_bounds(args)
-  if args.state.player_x +  PLAYER_W > args.grid.w
-    args.state.player_x = args.grid.w - PLAYER_W
+  if args.state.player.x +  args.state.player.w > args.grid.w
+    args.state.player.x = args.grid.w - args.state.player.w
   end
 
-  if args.state.player_x < 0
-    args.state.player_x = 0
+  if args.state.player.x < 0
+    args.state.player.x = 0
   end
 
-  if args.state.player_y + PLAYER_H > args.grid.h
-    args.state.player_y = args.grid.h - PLAYER_H
+  if args.state.player.y + args.state.player.h > args.grid.h
+    args.state.player.y = args.grid.h - args.state.player.h
   end
 
-  if args.state.player_y < 0
-    args.state.player_y = 0
+  if args.state.player.y < 0
+    args.state.player.y = 0
   end
 end
