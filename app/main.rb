62f42cee3fffe9e2f9e2fa74f6575c9c702b93df
Sam Markham <sam.markham@clio.com>
2024-02-29 11:08:06 -0800

    Player movement

--- app/main.rb
+++ app/main.rb
@@ -1,18 +1,37 @@
 def tick args
-  args.outputs.labels  << [640, 540, 'Hello World!', 5, 1]
-  args.outputs.labels  << [640, 500, 'Docs located at ./docs/docs.html and 100+ samples located under ./samples', 5, 1]
-  args.outputs.labels  << [640, 460, 'Join the Discord server! https://discord.dragonruby.org', 5, 1]
+  args.state.player_x ||= 120
+  args.state.player_y ||= 280
+  speed = 12
+  player_w = 100
+  player_h = 80
 
-  args.outputs.sprites << { x: 576,
-                            y: 280,
-                            w: 128,
-                            h: 101,
-                            path: 'dragonruby.png',
-                            angle: args.state.tick_count }
+  if args.inputs.left
+    args.state.player_x -= speed
+  elsif args.inputs.right
+    args.state.player_x += speed
+  end
 
-  args.outputs.labels  << { x: 640,
-                            y: 60,
-                            text: './mygame/app/main.rb',
-                            size_enum: 5,
-                            alignment_enum: 1 }
+  if args.inputs.up
+    args.state.player_y += speed
+  elsif args.inputs.down
+    args.state.player_y -= speed
+  end
+
+  if args.state.player_x +  player_w > args.grid.w
+    args.state.player_x = args.grid.w - player_w
+  end
+
+  if args.state.player_x < 0
+    args.state.player_x = 0
+  end
+
+  if args.state.player_y + player_h > args.grid.h
+    args.state.player_y = args.grid.h - player_h
+  end
+
+  if args.state.player_y < 0
+    args.state.player_y = 0
+  end
+
+  args.outputs.sprites << [args.state.player_x, args.state.player_y, player_w, player_h, 'sprites/misc/dragon-0.png']
 end
